7b1c82c7fedfdc8ad74d1ed64e46f09a4a994c78
Riya Kumari <riya.kumari.4rmk@gmail.com>
2025-02-08 11:45:28 +0530

    env reated

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const multer = require('multer');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+require('dotenv').config();
+const mongoUri=process.env.MONGODB_URI;
 app.use(cors());
 // Middleware to parse JSON and URL-encoded bodies
 app.use(bodyParser.json());
@@ -16,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/loginapp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.error('Error connecting to MongoDB:', error));
 
